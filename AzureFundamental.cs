34d854c007f2f765a34e2d022f2b625e9e999c6d
Rahul Pandey <rahulpandey862@gmail.com>
2024-07-21 05:47:05 +0530

    Basic API setup

---
+++ AzureFundamental.cs
@@ -0,0 +1,34 @@
+﻿namespace web_api;
+
+public class AzureFundamental
+{
+    public string TopicName { get; set; } = default!;
+
+    public string WeightageInPercentage { get; set; } = default!;
+}
+
+
+public class AzureFundamentalDescription
+{
+    public  IEnumerable<AzureFundamental> GetCourseList()
+    {
+        return new List<AzureFundamental>()
+        {
+            new AzureFundamental()
+            {
+                TopicName = "Cloud Concepts",
+                WeightageInPercentage = "25-30%"
+            },
+            new AzureFundamental()
+            {
+                TopicName = "Azure architecture and services",
+                WeightageInPercentage = "35-40%"
+            },
+            new AzureFundamental()
+            {
+                TopicName = "Azure management and governance",
+                WeightageInPercentage = "30-35%"
+            }
+        };
+    }
+}
